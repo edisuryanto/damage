c762236a86833c2e707ec092e059bc5ad666d735
EDI SURYANTO <lerengngunung2@gmail.com>
2025-06-30 07:49:30 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'd13fc2f5-3e05-4795-81eb-44143a09e552';
 
 const พร็อกซีไอพีs = ['149.129.250.8'];
 
