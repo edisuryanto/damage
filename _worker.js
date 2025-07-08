539a2da9a0511a7b36754ccad2bf549833e22a49
EDI SURYANTO <lerengngunung2@gmail.com>
2025-07-09 00:54:18 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'd13fc2f5-3e05-4795-81eb-44143a09e552';
 
-const พร็อกซีไอพีs = ['149.129.250.8' '43.218.77.16'];
+const พร็อกซีไอพีs = ['149.129.250.8'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
